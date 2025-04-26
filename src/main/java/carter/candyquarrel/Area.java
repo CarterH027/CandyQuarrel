c54c1a605a29f6a625d53ce049abaa99a0fa907e
Carter <carterwholt05@gmail.com>
2025-04-26 16:52:35 -0500

    Area now contains a builder to make creation easier.

--- src/main/java/carter/candyquarrel/Area.java
+++ src/main/java/carter/candyquarrel/Area.java
@@ -10,19 +10,12 @@ public class Area {
     private String name;
     private ArrayList<Area> connectedAreas;
 
-    public Area(String name, Item item, Enemy enemy){
-        this.name = name;
-        this.areaItem = item;
-        this.itemExists = true;
-        this.enemy = enemy;
-        this.enemyExists = true;
-    }
-
-    public Area(String name, Enemy enemy){
-        this.name = name;
-        this.itemExists = false;
-        this.enemy = enemy;
-        this.enemyExists = true;
+    public Area(AreaBuilder builder){
+        this.name = builder.name;
+        this.areaItem = builder.areaItem;
+        this.itemExists = builder.itemExists;
+        this.enemy = builder.enemy;
+        this.enemyExists = builder.enemyExists;
     }
 
     public void itemTaken(){
@@ -38,5 +31,30 @@ public class Area {
     @Override
     public String toString(){
         return name;
+    }
+    static class AreaBuilder{
+        private Item areaItem;
+        private boolean itemExists;
+        private Enemy enemy;
+        private boolean enemyExists;
+        private String name;
+
+        public AreaBuilder(String name){
+            this.name = name;
+            this.itemExists = false;
+            this.enemyExists = false;
+        }
+
+        public AreaBuilder setEnemy(Enemy enemy){
+            this.enemy = enemy;
+            this.enemyExists = true;
+            return this;
+        }
+
+        public AreaBuilder setItem(Item item){
+            this.areaItem = item;
+            this.itemExists = true;
+            return this;
+        }
     }
 }
